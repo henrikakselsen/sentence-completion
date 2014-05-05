1943509a4b77cab1ffd5cc6632e4163059aa809d
Henrik Akselsen <henrik.akselsen@gmail.com>
2014-05-05 08:00:56 +0200

    basic functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,15 @@ angular.module('sentenceApp')
       'AngularJS',
       'Karma'
     ];
+    $scope.sentences = [];
+    
+    $scope.addSentence = function(){
+      $scope.sentences.push($scope.currentStem + ' ' + $scope.newSentence);
+      $scope.newSentence = '';
+    };
+
+    $scope.saveAll = function(){
+      var blob = new Blob($scope.sentences, {type: "text/plain;charset=utf-8"});
+      saveAs(blob, $scope.currentStem + ".txt");
+    }
   });
